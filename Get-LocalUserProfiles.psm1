9604c21606087e2dbcf185d2932b70f5f60b55c4
mmseng <mmseng@users.noreply.github.com>
2021-01-30 01:42:06 -0600

    Update Get-LocalUserProfiles.psm1

--- Get-LocalUserProfiles.psm1
+++ Get-LocalUserProfiles.psm1
@@ -119,6 +119,12 @@ function Get-LocalUserProfiles {
 		$compName = $comp.Name
 		log "Getting profiles from `"$compName`"..." -L 1
 		$profiles = Get-CIMInstance -ComputerName $compName -ClassName "Win32_UserProfile"
+		
+		# Ignore system root profile
+		if(!$IncludeRootProfiles) {
+			$profiles = $profiles | Where { $_.LocalPath -notlike "*$env:SystemRoot*" }
+		}
+		
 		log "Found $($profiles.count) profiles." -L 2 -V 1
 		$comp | Add-Member -NotePropertyName "_Profiles" -NotePropertyValue $profiles -Force
 		Print-ProfilesFrom($comp)
@@ -199,11 +205,6 @@ function Get-LocalUserProfiles {
 		foreach($comp in $comps) {
 			$compName = $comp.Name
 			log "$compName" -L 1 -V 1
-			
-			# Ignore system root profile
-			if(!$IncludeRootProfiles) {
-				$comp._Profiles = $comp._Profiles | Where { $_.LocalPath -notlike "*$env:SystemRoot*" }
-			}
 			
 			# Track youngest and oldest profile
 			$oldestProfileDate = Get-Date # default to current date and time
